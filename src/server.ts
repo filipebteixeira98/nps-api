d255e5cb7701fd3a997d90bbfc44dd63c4ec7866
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-28 18:17:49 -0300

    chore: added middleware to handle with json & created router

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import express from 'express';
 
 import './database';
 
+import { router } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send();
-});
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3333, () =>
   console.log('🚀 Server is running at http://localhost:3333')
