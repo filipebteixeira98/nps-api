61685daf456475a9a281f94939c52f01e1cc97e5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-30 19:36:17 -0300

    test: should not be able to create a user with existing e-mail

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -19,4 +19,13 @@ describe('Users', () => {
 
     expect(response.status).toBe(201);
   });
+
+  it('should not be able to create a user with existing e-mail', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+    });
+
+    expect(response.status).toBe(400);
+  });
 });
