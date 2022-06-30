4391b7c672760900e476880877d634e830aa5190
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-30 18:10:27 -0300

    refactor: changed root archive

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,4 @@
-import 'reflect-metadata';
-import express from 'express';
-
-import './database';
-
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(router);
+import { app } from './app';
 
 app.listen(3333, () =>
   console.log('🚀 Server is running at http://localhost:3333')
