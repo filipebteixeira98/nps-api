129cd071bc3b1822b58bbc60e9420c3e607aa9df
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-30 17:07:29 -0300

    feat: added route to create surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
-import { UserController } from './controllers/UserController';
+import { UserController } from './controllers/UsersController';
+import { SurveysController } from './controllers/SurveysController';
 
 const router = Router();
 
 const usersController = new UserController();
+const surveysController = new SurveysController();
 
 router.post('/users', usersController.create);
+
+router.post('/surveys', surveysController.create);
 
 export { router };
