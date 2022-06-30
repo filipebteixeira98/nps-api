4a81a1553e5501e20080d6e1b4e9137582fb9760
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-30 19:24:07 -0300

    test: should be able to create a new user

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+
+import createConnection from '../database';
+
+import { app } from '../app';
+
+describe('Users', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+
+    await connection.runMigrations();
+  });
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
