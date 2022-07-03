e4f08c834d53be7675646c5a09d19f2223e734c6
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-07-03 17:49:19 -0300

    feat: added route that can create a survey & send an e-mail to users

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import { UserController } from './controllers/UsersController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const usersController = new UserController();
 const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post('/users', usersController.create);
 
 router.get('/surveys', surveysController.index);
 router.post('/surveys', surveysController.create);
+
+router.post('/sendMail', sendMailController.execute);
 
 export { router };
