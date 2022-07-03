1d2a791efab12ea397e372da54c402e6adef47cf
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-07-03 18:39:42 -0300

    feat: added compiled & parsed template for e-mail archive

--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,7 @@
+import fs from 'fs';
+import { resolve } from 'path';
 import nodemailer, { Transporter } from 'nodemailer';
+import handlebars from 'handlebars';
 
 class SendMailService {
   private client: Transporter;
@@ -20,10 +23,30 @@ class SendMailService {
   }
 
   async execute(to: string, subject: string, body: string) {
+    const templateMailPath = resolve(
+      __dirname,
+      '..',
+      'views',
+      'emails',
+      'npsMail.hbs'
+    );
+
+    const templateMailContent = fs
+      .readFileSync(templateMailPath)
+      .toString('utf8');
+
+    const templateMailParsed = handlebars.compile(templateMailContent);
+
+    const html = templateMailParsed({
+      name: to,
+      title: subject,
+      description: body,
+    });
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html,
       from: 'NPS <noreply@nps.com.br>',
     });
 
