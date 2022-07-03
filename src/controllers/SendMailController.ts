ecf1111cb9e69c3eae492e92d4a21a47683267e9
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-07-03 19:30:47 -0300

    chore: created relationship between entities in pivot table

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -48,6 +48,7 @@ class SendMailController {
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
       where: [{ user_id: userAlreadyExists.id }, { value: null }],
+      relations: ['user', 'survey'],
     });
 
     if (surveyAlreadyExists) {
